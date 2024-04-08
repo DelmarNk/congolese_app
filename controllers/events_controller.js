d81b0401da98a2d1c0bec2a7495d7cfd6ddf3bcc
delmarNK <delmarjinku3@gmail.com>
2024-04-09 01:15:03 +0800

    created the create route

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -5,11 +5,20 @@ const events = require('../models/Events')
 //get route
 router.get('/', async(req,res)=>{
     try{
-        const event = await events.find({})
-        res.status(200).json(event)
+        const events = await events.find({})
+        res.status(200).json(events)
     } catch(error){
         res.status(400)(error)
     }
 })
 
+//create route
+router.post('/', async(req,res)=>{
+    try{
+        const event = await events.create(req.body)
+        res.status(201).json(events)
+    } catch(error){
+        res.statut(400)(error)
+    }
+})
 
