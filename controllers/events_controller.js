6870de2ce2f4b166938ca81d444d106ac7ccdb83
delmarNK <delmarjinku3@gmail.com>
2024-04-09 01:07:09 +0800

    created the get route to all events

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -1,4 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const event = require('../models/event')
+const events = require('../models/Events')
+
+//get route
+router.get('/', async(req,res)=>{
+    try{
+        const event = await events.find({})
+        res.status(200).json(event)
+    } catch(error){
+        res.status(400)(error)
+    }
+})
+
 
