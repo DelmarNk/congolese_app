7d2207904d2e2cc3f54be97de598578b380c4c25
delmarNK <delmarjinku3@gmail.com>
2024-04-09 01:26:21 +0800

    created CRUD

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -22,3 +22,29 @@ router.post('/', async(req,res)=>{
     }
 })
 
+//show route
+router.get('/:id', async (req,res)=>{
+    try{
+        const event = await events.findById(req.params.id)
+        res.status(201).json(event)
+    } catch(error){
+        res.statut(400)(error)}
+})
+
+//delete route
+router.delete('/:id', async(req,res)=>{
+    try{
+        const deletedEvent = await events.findByIdAndDelete(req.params.id)
+        res.status(201).json(events)
+    } catch(error){
+        res.statut(400)(error)}
+})
+
+//update route
+router.put('/:id', async(req,res)=>{
+    try{
+        const newEvent = await events.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.staut(200).json(events)
+    } catch(error){
+        res.statut(400)(error)}
+})
