c80ed8fadf490bb5a1d13ee7bb412a83b7a68c0f
delmarNK <delmarjinku3@gmail.com>
2024-04-28 14:19:31 +0800

    created a post route in auth_controller

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -4,4 +4,14 @@ const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const {createUserToken, requireToken} = require('../middleware/auth')
 
-router.post('')
+router.post('/register', async(req,res)=>{
+    try{
+        const salt = await bcrypt.genSalt(12)
+        const passwordHarsh = await bcrypt.harsh(req.body.password, salt)
+        req.body.password = passwordHarsh
+        const user = await User.create(req.body)
+        res.status(201).json(user)
+    } catch(error){
+        res.status(400).json(error)
+    }
+})
